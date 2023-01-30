967fdca8f9ad0ef2012a962ffd3cb20338dd72ed
matt90800 <56865568+matt90800@users.noreply.github.com>
2023-01-30 21:37:03 +0100

    dÃébut travail compilateur

--- src/Export/Compilator.java
+++ src/Export/Compilator.java
@@ -1,34 +1,30 @@
 package Export;
 
 import java.util.ArrayList;
-
 import java.util.Objects;
 
 public class Compilator {
     protected String openTag;
     protected String closeTag;
-    protected int pos=0;
+    protected static int pos=0;
     protected String token= new String();
     protected String doc=new String();
 
+
     //getters
-    public String getOpenTag() {
+    public String getOpenTag () {
         return openTag;
     }
-
-    public String getCloseTag() {
+    public String getCloseTag () {
         return closeTag;
     }
 
-    public int getPos() {
+    public static int getPos() {
         return pos;
     }
-
     public String getToken() {
         return token;
-    }
-
-    public String getDoc() {
+    }    public String getDoc() {
         return doc;
     }
 
@@ -42,8 +38,8 @@ public class Compilator {
     public void setPos(int pos) {
         this.pos = pos;
     }
-    public void setToken(String token) {
-        this.token = token;
+    public String setToken(String token) {
+        return this.token = token;
     }
     public void setDoc(String doc) {
         this.doc = doc;
@@ -51,45 +47,25 @@ public class Compilator {
 
     //builders
     public Compilator() {
-            this.setOpenTag("<");
-            this.setCloseTag(">");
-        }
+        this.setOpenTag("<");
+        this.setCloseTag(">");
+    }
     public Compilator(String textToCompile) {
-            this.setOpenTag("<");
-            this.setCloseTag(">");
-            this.setDoc(textToCompile);
-        }
+        this.setOpenTag("<");
+        this.setCloseTag(">");
+        this.setDoc(textToCompile);
+    }
     public Compilator(String openTag, String closeTag) {
-            this.setCloseTag(closeTag);
-            this.setOpenTag(openTag);
-        }
+        this.setCloseTag(closeTag);
+        this.setOpenTag(openTag);
+    }
     public Compilator(String openTag, String closeTag,String textToCompile) {
-            this.setCloseTag(closeTag);
-            this.setOpenTag(openTag);
-            this.setDoc(textToCompile);
-        }
-
-    public static void  main(String[] args) {
-        Compilator comp = new Compilator("kdjfhfyy <dev> Hello world <dev/>");
-        debug(true);
-        comp.compile(comp.getDoc());
-    }
-    protected void cursor() {
-        setToken(String.valueOf(doc.charAt(getPos())));
-    }
-    public void next(){
-        this.pos++;
-        this.cursor();
-    }
-    public void previous(){
-        this.pos--;
-        this.cursor();
-    }
-    protected void reset(){
-        this.pos=0;
-        this.cursor();
+        this.setCloseTag(closeTag);
+        this.setOpenTag(openTag);
+        this.setDoc(textToCompile);
     }
 
+    //debug
     public static boolean debug=false;
     public static void debug(Boolean debug){
         Compilator.debug=debug;
@@ -102,44 +78,80 @@ public class Compilator {
         }
     }
 
+    //main pour fatih
+    public static void  main(String[] args) {
+        //   Compilator comp = new Compilator("kdjfhfyy <dev> Hello world <dev/>");
+        //  debug(false);
+        //   comp.compile(comp.getDoc());
+        Compilator code = new Compilator("kdjfhfyy <code> Hello world <code/>");
+        debug(true);
+        code.compile(code.getDoc());
+    }
+
+
+    protected String cursor() {
+        return setToken(String.valueOf(doc.charAt(getPos())));
+    }
+    public String next(){
+        this.pos++;
+        //retourne this.token
+        return this.cursor().toLowerCase();
+    }
+    public String previous(){
+        this.pos--;
+        this.cursor();
+        return this.getToken();
+    }
+    protected String reset(){
+        this.pos=0;
+        this.cursor();
+        return this.getToken();
+    }
+
     public /*ArrayList<Node>*/Node compile (String file){
         cursor();
         while (!Objects.equals(this.getToken(), this.getOpenTag())) {
             debug(token);
             next();
         }
-        debug(token);
-        return expr(null);
+
+        return expr( this.doc.substring(pos));
     }
-    private Node expr (Node expr) {
-        debug(token);
-        return null;
+    private Node expr (String expr) {
+        if (next().equals("c")) {
+            System.out.print(this.doc.substring(pos-1,pos+"code>".length()));
+            return new CodeNode();
         }
-
-
-    //méthodes
-    private Object exprend(){
+        if (next().equals("u")||next().equals("d"))
+            return new DocumentationNode();
+        debug(token);
         return null;
     }
 
-    private Object mainTag(){
-     return null;
-    }
 
-    private Object text(){
+    private Node exprend () {
         return null;
     }
 
-    private Object tag(){
+    private Node mainTag () {
         return null;
     }
 
-    private Object tagEnd(){
+    private Node text () {
         return null;
     }
 
-    private Object word(){
+    private Node tag () {
+        return null;
+    }
+
+    private Node tagEnd () {
+        return null;
+    }
+
+    private Node word () {
         return null;
     }
 
 }
+
